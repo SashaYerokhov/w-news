cae6e81e066288e06cdb5d0c6358a201524b5802
SashaYerokhov <126447651+SashaYerokhov@users.noreply.github.com>
2023-06-12 16:11:36 +0300

    Add files via upload

---
+++ w-news/js/burger.js
@@ -0,0 +1,18 @@
+// alert ('test')
+const btnLines = document.querySelector('.header__burger-lines')
+console.log (btnLines);
+const btnCross = document.querySelector('.header__burger-cross')
+console.log (btnCross);
+
+const navElement = document.querySelector ('nav')
+console.log (navElement);
+
+btnLines.addEventListener ('click', () => {
+    navElement.classList.add ('activ-menu')
+}
+)
+
+btnCross.addEventListener ('click', () => {
+    navElement.classList.remove ('activ-menu')
+} )
+
